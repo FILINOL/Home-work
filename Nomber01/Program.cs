8a2945bda7f5521eac05931b5d763a340ad736cd
FILIN <hagi_beter@mail.ru>
2023-04-10 11:11:13 +0300

    Nomber01

---
+++ Nomber01/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите превое число: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+
+Console.Write ("Введите второе число: ");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+
+
+if (number1 > number2 )
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(number1);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(number2);
+}
