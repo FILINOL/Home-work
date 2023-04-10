81148bd52d06a426f3e16e450734b5df4b35a691
FILIN <hagi_beter@mail.ru>
2023-04-10 12:04:35 +0300

    Nomber03

---
+++ Nomber03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите превое число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+
+if (number % 2 == 0)
+{
+    Console.Write("Число является четным");
+}
+else
+{
+    Console.Write("Число не является четным");
+}
