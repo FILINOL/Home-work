d89c2734b648eb4998451799343cb39a755fb33e
FILIN <hagi_beter@mail.ru>
2023-04-10 22:28:51 +0300

    Nomber04

---
+++ Nomber04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+int input = 2;
+
+while (input <= number)
+{
+    Console.WriteLine(input);
+    input = input + 2;
+}
